d7946c8362b8a9608c54e940991545730e272688
George <chartounygeorge@gmail.com>
2022-10-26 09:11:08 +0300

    saving token in asyncStorage

--- redux/apiLoginCalls.js
+++ redux/apiLoginCalls.js
@@ -1,13 +1,49 @@
 import { loginStart, loginFailure, loginSuccess } from "./userRedux";
 import { userRequest } from "../requestMethods";
+import  AsyncStorage  from "@react-native-async-storage/async-storage";
+
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
 
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem('@storage_Key', value)
+    } catch (e) {
+      // saving error
+      console.log("Cannot set token: ", e);
+    }
+  }
+
+
+const getData = async () => {
   try {
-    const response = await userRequest.post("/auth/login", user);
+    const value = await AsyncStorage.getItem('@storage_Key')
+    if(value !== null) {
+      // value previously stored
+      console.log("token access: ", value);
+    }
+  } catch(e) {
+    // error reading value
+    console.log("error geting token async");
+    console.log("ERRR: " , e);
+  }
+}
+
+
+  try {
+    const response = await userRequest.post("/auth/signin", user);
     dispatch(loginSuccess(response.data));
+    const token = response.data.accessToken;
+    console.log("response: " , response.data.accessToken)
+
+    storeData(token);
+    getData();
+
+
   } catch (error) {
     dispatch(loginFailure());
+    console.log("response fail ");
+    console.log("Error: " , error);
   }
 };
--- requestMethods.js
+++ requestMethods.js
@@ -1,8 +1,24 @@
 import axios from "axios";
-import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import  AsyncStorage  from "@react-native-async-storage/async-storage";
 
-console.log("Storage: ", AsyncStorage);
+
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('@storage_Key')
+    if(value !== null) {
+      // value previously stored
+      console.log("token access: ", value);
+      return value;
+    }
+  } catch(e) {
+    // error reading value
+    console.log("error geting token async");
+    console.log("ERRR: " , e);
+  }
+}
+getData();
 
 export const userRequest = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: 'http://34.245.213.76:3000',
 });
+  
