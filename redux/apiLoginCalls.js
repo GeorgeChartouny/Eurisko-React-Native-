c7b589dd11a7b57f6605de2044736f8a903436ce
George <chartounygeorge@gmail.com>
2022-10-27 10:17:27 +0300

    cleaning apiLoginCalls debuggers

--- redux/apiLoginCalls.js
+++ redux/apiLoginCalls.js
@@ -1,56 +1,52 @@
 import { loginStart, loginFailure, loginSuccess } from "./userRedux";
 import { userRequest } from "../requestMethods";
-import  AsyncStorage  from "@react-native-async-storage/async-storage";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const login = async (dispatch, user) => {
+  // call redux function loginStart
   dispatch(loginStart());
 
+  // storing token in AsyncStorage
   const storeData = async (value) => {
     try {
-      await AsyncStorage.setItem('@storage_Key', value)
+      await AsyncStorage.setItem("@storage_Key", value);
     } catch (e) {
-      // saving error
+      // logging error
       console.log("Cannot set token: ", e);
     }
-  }
+  };
 
-
-const getData = async () => {
-  try {
-    const value = await AsyncStorage.getItem('@storage_Key')
-    if(value !== null) {
-      // value previously stored
-      console.log("token access: ", value);
+  // fetching token from AsyncStorage
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@storage_Key");
+      if (value !== null) {
+        // value previously stored
+        console.log("token access: ", value);
+      }
+    } catch (e) {
+      // error reading value
+      console.log("error geting token async");
+      console.log("ERRR: ", e);
     }
-  } catch(e) {
-    // error reading value
-    console.log("error geting token async");
-    console.log("ERRR: " , e);
-  }
-}
+  };
 
+  // calling the api to login
   try {
     const response = await userRequest.post("/auth/signin", user);
- 
-    // if( data.error){
-    //   console.log("error fetchingg: " , data.message);
-    // }
-    // if(data.accessToken) {
 
-      dispatch(loginSuccess(response.data));
-      const token = response.data.accessToken;
-      console.log("token from loginSuccess: " , response.data.accessToken)
-    
-   
+    // send user data to login sucess *needs error handling
+    dispatch(loginSuccess(response.data));
+    const token = response.data.accessToken;
+    console.log("token from loginSuccess: ", response.data.accessToken);
 
+    // call the async function for storing token
     storeData(token);
     getData();
-    // }
-
   } catch (error) {
+    // trigger loginFailure from redux
     dispatch(loginFailure());
     console.log("response fail ");
-    console.log("Error: " , error);
+    console.log("Error: ", error);
   }
 };
