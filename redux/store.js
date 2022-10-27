24aa844401e8f59dc5b54ee7cc4731aa52d9f878
George <chartounygeorge@gmail.com>
2022-10-27 10:24:14 +0300

    cleaning store debuggers

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userRedux";
 
+// creating redux store and adding the reducers
 export const store = configureStore({
   reducer: {
     user: userSlice,
