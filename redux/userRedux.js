d35a0fdff039c9de744ec4c82ab26b33916cef17
George <chartounygeorge@gmail.com>
2022-10-27 10:26:24 +0300

    cleaning userRedux debuggers

--- redux/userRedux.js
+++ redux/userRedux.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// creating the user slice with initial states
 const userSlice = createSlice({
   name: "user",
   initialState: {
@@ -9,10 +10,12 @@ const userSlice = createSlice({
     message: null,
   },
 
+  //reducers triggered depending on the state
   reducers: {
     loginStart: (state) => {
       state.isFetching = true;
     },
+      // if credentials are correct, update the currentUser data, in this case only adding the accessToken
     loginSuccess: (state, action) => {
       state.isFetching = false;
       state.currenUser = action.payload;
