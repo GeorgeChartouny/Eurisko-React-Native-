3a8190c3d310990c07554cda89ce352e848857b4
George <chartounygeorge@gmail.com>
2022-10-29 09:06:08 +0300

    pull to refresh

--- screens/Article/ArticleMainPage.js
+++ screens/Article/ArticleMainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useEffect, useState } from "react";
 import {
   ActivityIndicator,
@@ -11,12 +11,19 @@ import {
   View,
   Button,
   FlatList,
+  SafeAreaView,
+  RefreshControl,
+  LogBox,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 // import { Button } from "react-native-paper";
 import { getArticle } from "../../redux/apiArticleCalls";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
 
 export const ArticleMainPage = ({ navigation }) => {
   const [page, setPage] = useState(0);
@@ -54,14 +61,6 @@ export const ArticleMainPage = ({ navigation }) => {
 
   const renderItem = ({ item, index }) => {
     return (
-      // <ScrollView>
-      //   <Text>{item?.headline.main}</Text>
-      //   <Text>{item.lead_paragraph}</Text>
-      //   <Text>BATATA</Text>
-      //   <Text>BATATA</Text>
-      //   <Text>BATATA</Text>
-      //   <Text>BATATA</Text>
-      // </ScrollView>
       <View key={index}>
         <TouchableOpacity
           activeOpacity={0.7}
@@ -118,147 +117,95 @@ export const ArticleMainPage = ({ navigation }) => {
   useEffect(() => {
     handleGetArticles();
   }, [page]);
+  useEffect(() => {
+    LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
+  }, []);
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   return (
     <>
-      <View style={styles.TopContainer}>
-        <View style={styles.SearchContainer}>
-          <TextInput
-            style={styles.SearchBar}
-            placeholder="Search for an article..."
-            onChangeText={(text) => {
-              setSearchTerm(text);
-            }}
-            value={searchTerm}
-          />
-          <TouchableOpacity>
-            <Button title="X" onPress={() => setSearchTerm("")}>
-              X
-            </Button>
-          </TouchableOpacity>
-        </View>
-        <Button
-          style={styles.button}
-          title="Logout"
-          mode="contained"
-          onPress={() => handleLogout()}
+      <SafeAreaView style={styles.container}>
+        <ScrollView
+          contentContainerStyle={styles.scrollView}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
         >
-          Logout
-        </Button>
-      </View>
-      {isFetching ? (
-        <ActivityIndicator
-          color="#137DC5"
-          size="large"
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        />
-      ) : (
-        <>
-          <FlatList
-            onEndReachedThreshold={0}
-            data={
-              articlesData &&
-              articlesData.filter((article) => {
-                if (searchTerm == "") {
-                  return article;
-                } else if (
-                  JSON.stringify(article.headline.main)
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase()) ||
-                  JSON.stringify(article.lead_paragraph)
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase())
-                ) {
-                  return article;
-                }
-              })
-            }
-            renderItem={renderItem}
-            onEndReached={() =>
-              searchTerm == "" ? setPage(page == 2 ? page : page + 1) : null
-            }
-            // keyExtractor={(item) => item._id}
-          />
+          <Text>Pull down to batata</Text>
 
-          {/* <ScrollView style={{ backgroundColor: "#f0f0f0" }}>
-            {articles &&
-              articles
-                .filter((article) => {
-                  if (searchTerm == "") {
-                    return article;
-                  } else if (
-                    JSON.stringify(article.headline.main)
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase()) ||
-                    JSON.stringify(article.lead_paragraph)
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase())
-                  ) {
-                    return article;
-                  }
-                })
-                .map((article, index) => {
-                  return (
-                    <View key={article._id}>
-                      <TouchableOpacity
-                        activeOpacity={0.7}
-                        key={index}
-                        onPress={() =>
-                          navigation.navigate("ArticlePage", {
-                            selectedArticle: article,
-                          })
-                        }
-                      >
-                        <View style={styles.cardContainer}>
-                          <View>
-                            {article.multimedia.length === 0 ? (
-                              <Image
-                                style={styles.cardImage}
-                                source={require("../../assets/No_Image.png")}
-                                resizeMode="cover"
-                              />
-                            ) : (
-                              article.multimedia.map((img, index) => {
-                                return (
-                                  index === 0 && (
-                                    <Image
-                                      key={index}
-                                      style={styles.cardImage}
-                                      source={{
-                                        uri: `https://static01.nyt.com/${img.url}`,
-                                      }}
-                                      resizeMode="cover"
-                                    />
-                                  )
-                                );
-                              })
-                            )}
-                          </View>
-                          <View style={styles.contentInfo}>
-                            <Text style={styles.titleMain}>
-                              {article.headline.main}
-                            </Text>
-                            <View style={styles.bottomCard}>
-                              <Text style={styles.publisherName}>
-                                {article.byline.original
-                                  ? article.byline.original
-                                  : "Name Not Available"}
-                              </Text>
-                              <Text style={styles.publishDate}>
-                                - Published on{" "}
-                                {JSON.stringify(article.pub_date).slice(1, 11)}
-                              </Text>
-                            </View>
-                          </View>
-                        </View>
-                      </TouchableOpacity>
-                    </View>
-                  );
-                })}
-          </ScrollView> */}
-        </>
-      )}
-      {error && <Text style={styles.ErrorMessage}> {message}</Text>}
+          <View style={styles.TopContainer}>
+            <View style={styles.SearchContainer}>
+              <TextInput
+                style={styles.SearchBar}
+                placeholder="Search for an article..."
+                onChangeText={(text) => {
+                  setSearchTerm(text);
+                }}
+                value={searchTerm}
+              />
+              <TouchableOpacity>
+                <Button title="X" onPress={() => setSearchTerm("")}>
+                  X
+                </Button>
+              </TouchableOpacity>
+            </View>
+            <Button
+              style={styles.button}
+              title="Logout"
+              mode="contained"
+              onPress={() => handleLogout()}
+            >
+              Logout
+            </Button>
+          </View>
+          {isFetching ? (
+            <ActivityIndicator
+              color="#137DC5"
+              size="large"
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            />
+          ) : (
+            <>
+              <FlatList
+                onEndReachedThreshold={0}
+                data={
+                  articlesData &&
+                  articlesData.filter((article) => {
+                    if (searchTerm == "") {
+                      return article;
+                    } else if (
+                      JSON.stringify(article.headline.main)
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      JSON.stringify(article.lead_paragraph)
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                    ) {
+                      return article;
+                    }
+                  })
+                }
+                renderItem={renderItem}
+                onEndReached={() =>
+                  searchTerm == "" ? setPage(page == 2 ? page : page + 1) : null
+                }
+                // keyExtractor={(item) => item._id}
+              />
+            </>
+          )}
+          {error && <Text style={styles.ErrorMessage}> {message}</Text>}
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 };
